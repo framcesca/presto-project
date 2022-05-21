f24ceec5b30c32ba13f481bec3aeeb2cc95255de
Francesca Mancini <francescamancini096@gmail.com>
2022-05-21 20:12:16 +0200

    sezione contatti finita

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// CAROSELLO
 const swiper = new Swiper('.last-prod-carousel', {
     // Optional parameters
     slidesPerView: 1,
@@ -25,8 +26,27 @@ const swiper = new Swiper('.last-prod-carousel', {
             slidesPerView: 3,
             spaceBetween: 75,
         },
-    },
-
-
-    
+    },    
 });
+
+
+// validation form
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
